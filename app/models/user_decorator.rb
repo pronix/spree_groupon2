7bd5e15293cd81d05207d04f67492029de91a3ee
dimanlin <devandart@ya.ru>
2011-04-26 17:54:11 +0400

    add prefix "_decorator" for controllers and models

--- app/models/user_decorator.rb
+++ app/models/user_decorator.rb
@@ -1,7 +1,10 @@
-User.class_eval do 
+User.class_eval do
+
   validates_presence_of :phone, :region_id
   belongs_to :region
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :persistence_token, :phone, :region_id
+
 end
+
