58994f54ed5821fa78b441d3c248c84802d0ac4a
dimanlin <devandart@ya.ru>
2011-04-26 14:14:48 +0400

    Переделанные модели

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,18 +1,3 @@
-class Country < ActiveRecord::Base
-  has_many :states
-
-  has_one :zone_member, :as => :zoneable
-  has_one :zone, :through => :zone_member
-
+Country.class_eval do 
   has_many :regions
-
-  validates :name, :iso_name, :presence => true
-
-  def <=>(other)
-    name <=> other.name
-  end
-
-  def to_s
-    name
-  end
 end
