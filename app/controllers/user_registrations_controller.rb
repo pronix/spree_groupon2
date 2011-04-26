5316c9fe1a02e1ab8707abb2e562c67c941a49e8
dimanlin <devandart@ya.ru>
2011-04-26 17:15:39 +0400

    Поправил контроллеры

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -1,23 +1,10 @@
-class UserRegistrationsController < Devise::RegistrationsController
-  include SpreeBase
-  helper :users, 'spree/base'
-
-  ssl_required
-  after_filter :associate_user, :only => :create
-  before_filter :check_permissions, :only => [:edit, :update]
-  skip_before_filter :require_no_authentication
+UserRegistrationsController.class_eval do 
   before_filter :init_regions, :only => [:new]
 
   def confirm_phone
     @user = User.find_by_email(params[:email])
   end
 
-  # GET /resource/sign_up
-  def new
-    super
-  end
-
-  # POST /resource/sign_up
   def create
     @user = build_resource(params[:user])
     logger.debug(@user)
@@ -33,44 +20,11 @@ class UserRegistrationsController < Devise::RegistrationsController
     end
   end
 
-  # GET /resource/edit
-  def edit
-    super
-  end
-
-  # PUT /resource
-  def update
-    super
-  end
-
-  # DELETE /resource
-  def destroy
-    super
-  end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  def cancel
-    super
-  end
-
-  protected
+  private
 
   def init_regions
     @regions = Country.find_by_iso("RU").regions
   end
 
-  def check_permissions
-    authorize!(:create, resource)
-  end
-
-  def associate_user
-    return unless current_user and current_order
-    current_order.associate_user!(current_user)
-    session[:guest_token] = nil
-  end
-
 end
+
