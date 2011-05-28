caa928609360e6d71fd58f30a852353cf39c449d
Andrey V. Martyanov <andrey.v.martyanov@gmail.com>
2011-05-28 16:12:35 +0500

    Conditions and features on coupon creation page

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -2,13 +2,17 @@ Product.class_eval do
 
   has_many :conditions, :dependent => :destroy
   has_many :features, :dependent => :destroy
-  has_one :state
+  #has_one :state
 
   attr_accessible :name, :description, :price, :discount, :available_on, :sku,
-    :state_id, :permalink, :conditions_attributes, :features_attributes
+    :state_id, :permalink, :conditions, :conditions_attributes, :features, :features_attributes
 
-  validates_presence_of :discount, :state_id
+  validates_presence_of :discount, :state_id, :description,
+    :conditions, :features
 
-  accepts_nested_attributes_for :conditions, :features,
+  accepts_nested_attributes_for :conditions,
     :allow_destroy => true
+  accepts_nested_attributes_for :features,
+    :allow_destroy => true
+
 end
