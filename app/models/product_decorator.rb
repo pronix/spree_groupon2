e03e5522a1a42c5aca74d4afd3ea347ae5ff03ca
Andrey V. Martyanov <andrey.v.martyanov@gmail.com>
2011-05-29 02:38:18 +0500

    Added on hand property

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -3,13 +3,15 @@ Product.class_eval do
   has_many :conditions, :dependent => :destroy
   has_many :features, :dependent => :destroy
 
-  attr_accessible :name, :description, :price, :discount, :available_on, :sku,
+  attr_accessible :name, :description, :price, :discount, :available_on, :sku, :on_hand,
     :state_id, :permalink, :conditions, :conditions_attributes, :features, :features_attributes
 
-  validates_presence_of :discount, :state_id, :description
+  validates_presence_of :discount, :state_id, :description, :on_hand
+  validates_numericality_of :on_hand, :only_integer => true, :greater_than => 0,
+    :message => :must_be_greater_than_zero
 
-  validate :presence_of_features
   validate :presence_of_conditions
+  validate :presence_of_features
 
   accepts_nested_attributes_for :conditions,
     :allow_destroy => true
@@ -20,13 +22,13 @@ Product.class_eval do
 
   def presence_of_conditions
     if conditions.empty? or conditions.all? {|condition| condition.marked_for_destruction? }
-      errors[:base] << :must_have_at_least_one_condition
+      errors[:base] << I18n.t(:must_have_at_least_one_condition)
     end
   end
 
   def presence_of_features
     if features.empty? or features.all? {|feature| feature.marked_for_destruction? }
-      errors[:base] << :must_have_at_least_one_feature
+      errors[:base] << I18n.t(:must_have_at_least_one_feature)
     end
   end
 
